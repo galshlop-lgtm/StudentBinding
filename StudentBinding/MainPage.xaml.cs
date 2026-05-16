bcd01848603252cbbd0f7e35dc298d82c9772b1e
User <User@LAPTOP-I6VED7PO>
2026-05-17 00:12:36 +0300

    סיום משימה

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -1,12 +1,39 @@
-﻿namespace StudentBinding
+﻿using Microsoft.Maui.Controls;
+using StudentBinding.models;
+using System;
+using StudentBinding.models; 
+
+namespace StudentBinding 
 {
     public partial class MainPage : ContentPage
     {
-        
+       
+        public Student Student1 { get; set; }
+        public Student Student2 { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+          
+            Student1 = new Student { Name = "דניאל כהן", Image = "daniel.png" };
+            Student2 = new Student { Name = "נועה כהן", Image = "noa.png" };
+
+            
+            this.BindingContext = Student1;
         }
 
+       
+        private void OnChangeStudentClicked(object sender, EventArgs e)
+        {
+            if (this.BindingContext == Student1)
+            {
+                this.BindingContext = Student2;
+            }
+            else
+            {
+                this.BindingContext = Student1;
+            }
+        }
     }
 }
---
+++ StudentBinding/models/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StudentBinding.models
+{
+    public class Student : ObservableObject
+    {
+        private string _name;
+        private string _image;
+        private DateTime _birthDate = DateTime.Now; 
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                if (_image != value)
+                {
+                    _image = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set
+            {
+                if (_birthDate != value)
+                {
+                    _birthDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
